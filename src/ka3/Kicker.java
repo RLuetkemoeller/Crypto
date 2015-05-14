81a274ac7cd7446a13f6ff06e68e79d57c23a553
FloppyDisck <eruetz.its-b2013@fh-salzburg.ac.at>
2015-05-14 13:47:12 +0200

    Added Kicker for correct usage of RSA class and comments for finding error; Encryption/Decryption not working, Euklid correct, Encryption/Decryption correct, Keygen correct

---
+++ src/ka3/Kicker.java
@@ -0,0 +1,11 @@
+package ka3;
+
+public class Kicker {
+
+	public static void main(String[] args) {
+		
+		RSA key = new RSA();
+		key.bugfix();
+	}
+
+}
--- src/ka3/RSA.java
+++ src/ka3/RSA.java
@@ -5,54 +5,54 @@ import java.security.SecureRandom;
 
 public class RSA {
 
+	// Es sollte für jede Verschlüsselung ein Keypaar erstellt werden.
+	// Um dies zu ermöglichen wird pro Keypaar eine RSA Klasse davon instanziert
 	private BigInteger d=null;
 	public BigInteger N=null, e=null;
 	
-	public static void main(String[] args) {
+	// Funktion zum Bugfixen, sobald Verschlüsselung läuft wird diese gelöscht
+	public void bugfix() {
 		String str = "Solo";
 		
-		RSA keyone = new RSA();
-		keyone.createKeys();
-		System.out.println("N: " + keyone.N + "; e: " + keyone.e + "; d: " + keyone.d);
-		
+		this.createKeys();
+		System.out.println("N: " + this.N + "; e: " + this.e + "; d: " + this.d);
 		
 		System.out.println("Message: " + str);
-		str = keyone.encrypt("Solo");
+		str = this.encrypt("Solo");
 		System.out.println("Encrypted: " + str);
-		str = keyone.decrypt(str);
+		str = this.decrypt(str);
 		System.out.println("Decrypted: " + str);
 		
 	}
 	
+	// Funktion zum Erstellen der Keys innerhalb dieser Instanz
 	public void createKeys(){
-		BigInteger p = getPrime(), q = getPrime(), phi=null;
+		BigInteger p = getPrime(), q = getPrime(), phi=null; // p und q initialisieren
 		
-		N = p.multiply(q);
-		phi = (p.add(new BigInteger("-1"))).multiply((q.add(new BigInteger("-1"))));
+		N = p.multiply(q); // N berechnen
+		phi = (p.add(new BigInteger("-1"))).multiply((q.add(new BigInteger("-1")))); // Phi berechnen
 		
-		do{
+		do{ // e erstellen, falls ein falsches erstellt wird, erneut abfragen (Vorgabe e > 0 und e < phi
 			e = getPrime();
 		} while(e.compareTo(new BigInteger("0")) < 0 || e.compareTo(phi) >= 0);
 		
-		while (phi.gcd(e) == new BigInteger("1")){
+		while (phi.gcd(e) == new BigInteger("1")){ // e auf gcd mit phi prüfen, falls gcd nicht 1, erneut e generieren
 			e = getPrime();
 		}
 		
-		
-		
-		System.out.println("phi: " + phi);
-		
-		
+		// ################################################
+		System.out.println("phi: " + phi); // Debug Zeile #
+		// ################################################
 		
 		BigInteger[] erg = new BigInteger[2];
-		erg = ExtEukl(phi, e);
+		erg = ExtEukl(phi, e); // Euklidscher Algorithmus durchlaufen
 		
-		if (erg == null){
+		if (erg == null){ // Check ob erg != null
 			System.out.println("ERROR: Euklid got wrong value. Pop out ...");
 			return;
 		}
 		
-		if (erg[1].multiply(phi).add(erg[0].multiply(e)) == new BigInteger("1")){
+		if (erg[1].multiply(phi).add(erg[0].multiply(e)) == new BigInteger("1")){ // Die übergebenen Werte sortieren, um die richtige Zahl für d zu bekommen
 		}
 		else{
 			BigInteger cache;
@@ -61,7 +61,7 @@ public class RSA {
 			erg[0] = cache;
 		}
 		
-		if (erg[0].compareTo(new BigInteger("0")) < 0){
+		if (erg[0].compareTo(new BigInteger("0")) < 0){ // Abfrage ob d negativ, wenn ja, dann -d + phi
 		d=erg[0].add(phi);
 		}
 		
@@ -83,50 +83,54 @@ public class RSA {
 	}
 	*/
 	
+	// Primzahlgenerator
 	private static BigInteger getPrime(){
 		int maxBitLength = 10; // Größe der Primzahlen
 		SecureRandom rand = new SecureRandom();
 		int n;
-		for (n = rand.nextInt(maxBitLength); n < 2; n = rand.nextInt(maxBitLength)){
+		for (n = rand.nextInt(maxBitLength); n < 2; n = rand.nextInt(maxBitLength)){ // n darf nicht kleiner als 2 sein für probablePrime
 		}
-		BigInteger prime = BigInteger.probablePrime(n, rand);
+		BigInteger prime = BigInteger.probablePrime(n, rand); // Generierung der Primzahl
 		return prime;
 	}
 	
+	// Selbstgeschriebener Euklid Algorithmus
 	private static BigInteger[] ExtEukl(BigInteger a, BigInteger b){
 		BigInteger[] arr = new BigInteger[2];
 		BigInteger cache;
 		
-		if (a.compareTo(b) < 0 || a.compareTo(new BigInteger("0")) < 0 || b.compareTo(new BigInteger("0")) < 0){
+		if (a.compareTo(b) < 0 || a.compareTo(new BigInteger("0")) < 0 || b.compareTo(new BigInteger("0")) < 0){ // Check für unpassende Parameter in der 1. Funktion
 			System.out.println("ERROR: Wrong parameter values.");
 			return null;
 		}
 		
-		if(b.compareTo(new BigInteger("0")) <= 0){
+		if(b.compareTo(new BigInteger("0")) <= 0){ // Abbruchbedingung für die rekursive Funktion
 			arr[0] = new BigInteger("1");
 			arr[1] = new BigInteger("0");
 			return arr;
 		}
 		
-		arr = ExtEukl(b, a.mod(b));
-		arr[0] = arr[1].multiply(a.divide(b).negate()).add(arr[0]);
-		cache = arr[1];
+		arr = ExtEukl(b, a.mod(b)); // Start der Rekursion
+		arr[0] = arr[1].multiply(a.divide(b).negate()).add(arr[0]); // Berechnung der Parameter d und k pro Step
+		cache = arr[1]; // Vertauschen der Variablen für rekursive Übergabe.
 		arr[1] = arr[0];
 		arr[0] = cache;
 		
 		return arr;
 	}
 	
+	// VerschlüsselungsFunktion
 	public String encrypt(String str){
-		BigInteger strInt = new BigInteger(str.getBytes()), code;
-		code = strInt.modPow(e, N);
+		BigInteger message = new BigInteger(str.getBytes()), code;
+		code = message.modPow(e, N);
 		str = new String(code.toByteArray());
 		return str;
 	}
 	
+	// Entschlüsselungsfunktion
 	public String decrypt(String str){
-		BigInteger strInt = new BigInteger(str.getBytes()), message;
-		message = strInt.modPow(d, N);
+		BigInteger code = new BigInteger(str.getBytes()), message;
+		message = code.modPow(d, N);
 		str = new String(message.toByteArray());
 		return str;
 	}
